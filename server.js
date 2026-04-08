e4d98dbf6463812aa2fd54f6747d2c0171d5083d
Shreya754 <145770088+Shreya754@users.noreply.github.com>
2026-04-08 23:44:44 +0530

    server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
 
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const fetch = require("node-fetch");
 
 const app = express();
 
